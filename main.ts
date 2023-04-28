ede8c52e6c776736a4389f506db70c37243c59c7
bocuypers <123274149+bocuypers@users.noreply.github.com>
2023-04-28 14:15:25 +0200

    AFSTANDSBEDIENING7

--- main.ts
+++ main.ts
@@ -5,54 +5,33 @@ let links = 11
 let rechts = 25
 let stop = 94
 basic.forever(function () {
-    ir_waarde = IR.IR_read()
-})
-basic.forever(function () {
     if (ir_waarde == stop) {
-        led.stopAnimation()
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(100)
     }
 })
 basic.forever(function () {
+    ir_waarde = IR.IR_read()
+})
+basic.forever(function () {
     if (ir_waarde == vooruit) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
     }
 })
 basic.forever(function () {
     if (ir_waarde == achteruit) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 30)
     }
 })
 basic.forever(function () {
     if (ir_waarde == links) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 30)
     }
 })
 basic.forever(function () {
     if (ir_waarde == rechts) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 30)
     }
 })
